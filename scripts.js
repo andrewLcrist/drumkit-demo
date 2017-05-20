fd20a94bdb2678fe198282b18dca4840c689b02b
Andrew Crist <andrew.l.crist@gmail.com>
2017-05-20 07:21:47 -0500

    Style changes

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const keys = document.querySelectorAll('.key')
+
 window.addEventListener('keydown', function(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
